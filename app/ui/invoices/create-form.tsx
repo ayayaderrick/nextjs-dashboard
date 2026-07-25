b568eb4c58f9f6d1b52b0fcc0738ed178673994f
ayayaderrick <ayayaderrick@protonmail.com>
2026-07-25 12:51:42 +0300

    feat(invoice): add unique keys to error message elements for customer, amount, and status

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -46,7 +46,9 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
           <div id="customer-error" aria-live="polite" aria-atomic="true">
             {state.errors?.customerId &&
               state.errors.customerId.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500">{error}</p>
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
               ))}
           </div>
         </div>
@@ -72,7 +74,9 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             <div id="amount-error" aria-live="polite" aria-atomic="true">
               {state.errors?.amount &&
                 state.errors.amount.map((error: string) => (
-                  <p className="mt-2 text-sm text-red-500">{error}</p>
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
                 ))}
             </div>
           </div>
@@ -122,13 +126,17 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
           <div id="status-error" aria-live="polite" aria-atomic="true">
             {state.errors?.status &&
               state.errors.status.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500">{error}</p>
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
               ))}
           </div>
         </fieldset>
         <div id="customer-error" aria-live="polite" aria-atomic="true">
           {state.message && (
-            <p className="mt-2 text-sm text-red-500">{state.message}</p>
+            <p className="mt-2 text-sm text-red-500" key={state.message}>
+              {state.message}
+            </p>
           )}
         </div>
       </div>
